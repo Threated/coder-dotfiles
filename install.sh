2bde75831b2a283aa6e3be78f3382fd624c105c0
Jan <59206115+Threated@users.noreply.github.com>
2023-03-25 15:32:52 +0100

    Symlink dotfiles

--- install.sh
+++ install.sh
@@ -7,6 +7,25 @@ source /verbis/functions.sh
 verbis_defaults_main
 verbis_defaults_rust
 
+# Yanked from https://github.com/bstollnitz/dotfiles/blob/main/install.sh
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
+
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
+
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
+}
+
+create_symlinks
+
 curl -L -o out.tgz https://github.com/cargo-bins/cargo-binstall/releases/latest/download/cargo-binstall-x86_64-unknown-linux-musl.tgz
 tar -xf out.tgz
 mv cargo-binstall $HOME/.cargo/bin
