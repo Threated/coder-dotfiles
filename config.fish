04b5141eb5a64bcce0495a4e3ccc3bf4f669bd39
Jan <59206115+Threated@users.noreply.github.com>
2023-12-15 22:48:15 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -3,3 +3,9 @@ fish_add_path ~/.cargo/bin/
 export STARSHIP_CONFIG=$HOME/.config/coderv2/dotfiles/starship.toml
 set -g fish_greeting
 starship init fish | source
+
+alias py 'python3'
+alias cat 'bat'
+alias rgf 'rg --files | rg'
+alias lsa 'ls -la'
+alias dockerip "docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}'"
